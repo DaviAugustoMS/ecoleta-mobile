5ee53e486775045070db692b4d342d6ba4c1fa2f
Davi Augusto <daviaugusto.developer@gmail.com>
2020-06-16 15:24:48 -0300

    alter

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import { Feather as Icon, FontAwesome } from "@expo/vector-icons";
-import * as MailComposer from "expo-mail-composer";
+import React, { useState, useEffect } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import * as MailComposer from 'expo-mail-composer'
 import {
   View,
   StyleSheet,
@@ -10,61 +10,61 @@ import {
   Image,
   SafeAreaView,
   Linking,
-} from "react-native";
-import Constants from "expo-constants";
-import { RectButton } from "react-native-gesture-handler";
-import api from "../../services/api";
+} from 'react-native'
+import Constants from 'expo-constants'
+import { RectButton } from 'react-native-gesture-handler'
+import api from '../../services/api'
 
 interface Params {
-  point_id: number;
+  point_id: number
 }
 
 interface Data {
   point: {
-    image: string;
-    name: string;
-    email: string;
-    whatsapp: string;
-    city: string;
-    uf: string;
-  };
+    image: string
+    name: string
+    email: string
+    whatsapp: string
+    city: string
+    uf: string
+  }
   items: {
-    title: string;
-  }[];
+    title: string
+  }[]
 }
 
 const Detail = () => {
-  const navigation = useNavigation();
-  const [data, setData] = useState<Data>({} as Data);
-  const route = useRoute();
+  const navigation = useNavigation()
+  const [data, setData] = useState<Data>({} as Data)
+  const route = useRoute()
 
-  const routeParams = route.params as Params;
+  const routeParams = route.params as Params
 
   useEffect(() => {
     api.get(`points/${routeParams.point_id}`).then((response) => {
-      setData(response.data);
-    });
-  }, []);
+      setData(response.data)
+    })
+  }, [])
 
   function handleNavigateBack() {
-    navigation.goBack();
+    navigation.goBack()
   }
 
   function handleComposeMail() {
     MailComposer.composeAsync({
-      subject: "Enteresse na coleta de resíduos",
+      subject: 'Enteresse na coleta de resíduos',
       recipients: [data.point.email],
-    });
+    })
   }
 
   function handleWhatsapp() {
     Linking.openURL(
-      `whatsapp://send?phone=${data.point.whatsapp}&text=Tenha interesse sobre coleta de resíduos`
-    );
+      `whatsapp://send?phone=${data.point.whatsapp}&text=Tenha interesse sobre coleta de resíduos`,
+    )
   }
 
   if (!data.point) {
-    return null;
+    return null
   }
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -81,7 +81,7 @@ const Detail = () => {
         />
         <Text style={styles.pointName}>{data.point.name}</Text>
         <Text style={styles.pointItems}>
-          {data.items.map((item) => item.title).join(", ")}
+          {data.items.map((item) => item.title).join(', ')}
         </Text>
 
         <View style={styles.address}>
@@ -102,8 +102,8 @@ const Detail = () => {
         </RectButton>
       </View>
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -113,26 +113,26 @@ const styles = StyleSheet.create({
   },
 
   pointImage: {
-    width: "100%",
+    width: '100%',
     height: 120,
-    resizeMode: "cover",
+    resizeMode: 'cover',
     borderRadius: 10,
     marginTop: 32,
   },
 
   pointName: {
-    color: "#322153",
+    color: '#322153',
     fontSize: 28,
-    fontFamily: "Ubuntu_700Bold",
+    fontFamily: 'Ubuntu_700Bold',
     marginTop: 24,
   },
 
   pointItems: {
-    fontFamily: "Roboto_400Regular",
+    fontFamily: 'Roboto_400Regular',
     fontSize: 16,
     lineHeight: 24,
     marginTop: 8,
-    color: "#6C6C80",
+    color: '#6C6C80',
   },
 
   address: {
@@ -140,43 +140,43 @@ const styles = StyleSheet.create({
   },
 
   addressTitle: {
-    color: "#322153",
-    fontFamily: "Roboto_500Medium",
+    color: '#322153',
+    fontFamily: 'Roboto_500Medium',
     fontSize: 16,
   },
 
   addressContent: {
-    fontFamily: "Roboto_400Regular",
+    fontFamily: 'Roboto_400Regular',
     lineHeight: 24,
     marginTop: 8,
-    color: "#6C6C80",
+    color: '#6C6C80',
   },
 
   footer: {
     borderTopWidth: StyleSheet.hairlineWidth,
-    borderColor: "#999",
+    borderColor: '#999',
     paddingVertical: 20,
     paddingHorizontal: 32,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 
   button: {
-    width: "48%",
-    backgroundColor: "#34CB79",
+    width: '48%',
+    backgroundColor: '#34CB79',
     borderRadius: 10,
     height: 50,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   buttonText: {
     marginLeft: 8,
-    color: "#FFF",
+    color: '#FFF',
     fontSize: 16,
-    fontFamily: "Roboto_500Medium",
+    fontFamily: 'Roboto_500Medium',
   },
-});
+})
 
-export default Detail;
+export default Detail
